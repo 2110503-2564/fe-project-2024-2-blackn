a03618290327f4c8ddac236e912bd244c1428503
ThawinXD <bto2168@gmail.com>
2025-03-21 13:57:32 +0700

    Create updateBooking.tsx

---
+++ src/libs/(booking)/updateBooking.tsx
@@ -0,0 +1,16 @@
+export default async function updateBooking(token: string, bookingId: string, booking: Booking) {
+   const res = await fetch(`${process.env.BACKEND_URL}/api/v1/bookings/${bookingId}`, {
+      method: "PUT",
+      headers: {
+         authorization: `Bearer ${token}`,
+         "Content-Type": "application/json"
+      },
+      body: JSON.stringify(booking)
+   });
+
+   if(!res.ok) {
+      throw new Error("Failed to update a booking");
+   }
+
+   return await res.json();
+}
