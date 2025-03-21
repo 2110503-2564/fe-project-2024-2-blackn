eed4b662d3069b2e6419b56eb3e9bdc837147b23
ThawinXD <bto2168@gmail.com>
2025-03-21 14:17:23 +0700

    Create userLogout.tsx

---
+++ src/libs/(auth)/userLogout.tsx
@@ -0,0 +1,14 @@
+export default async function userLogout(token: string) {
+   const res = await fetch(`${process.env.BACKEND_URL}/auth/logout`, {
+      method: "POST",
+      headers: {
+         authorization: `Bearer ${token}`,
+      }
+   });
+
+   if(!res.ok) {
+      throw new Error("Failed to logout user");
+   }
+
+   return await res.json();
+}
