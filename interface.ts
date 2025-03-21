3f8e6e1eed8a6d1465f3f13775dd71ae3195a5d5
ThawinXD <bto2168@gmail.com>
2025-03-21 13:32:16 +0700

    add interface

---
+++ interface.ts
@@ -0,0 +1,45 @@
+interface Booking {
+   id: string;
+   _id: string;
+   bookingDate: string;
+   user: User[];
+   dentist: Dentist[];
+   createdAt: string;
+   __v: number;
+}
+
+interface Dentist {
+   id: string;
+   _id: string;
+   name: string;
+   year_of_experience: number;
+   area_of_expertise: string;
+   bookings: Booking[];
+   createdAt: string;
+   __v: number;
+}
+
+interface UserFetch {
+   name: string;
+   email: string;
+   tel: string;
+   role: string;
+}
+interface User {
+   id: string;
+   _id: string;
+   name: string;
+   email: string;
+   tel_number: string;
+   role: string;
+   __v: number;
+}
+
+interface Log {
+   _id: string;
+   user: UserFetch;
+   booking: Booking;
+   actionType: string;
+   createdAt: string;
+   __v: number;
+}
