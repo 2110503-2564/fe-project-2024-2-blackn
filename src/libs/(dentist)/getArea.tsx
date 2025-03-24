63f485bac8599f07a9f919cea0441547864e4589
ThawinXD <bto2168@gmail.com>
2025-03-24 16:00:29 +0700

    add Search dentist

---
+++ src/libs/(dentist)/getArea.tsx
@@ -0,0 +1,9 @@
+export default async function getArea() {
+   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/dentists/area_of_expertise`, {next: {tags:['area']}});
+
+   if(!res.ok) {
+      throw new Error("Failed to fetch area");
+   }
+
+   return await res.json();
+}
