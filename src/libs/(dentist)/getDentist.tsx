cdb63ae5c5d18a269776050ba5239b4b8e034334
wishshukorn <hirhunwish@gmail.com>
2025-03-25 17:41:18 +0700

    code prettier delete/getArea/get dentist

--- src/libs/(dentist)/getDentist.tsx
+++ src/libs/(dentist)/getDentist.tsx
@@ -1,9 +1,11 @@
 export default async function getDentist(DentistId: string) {
-   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/dentists/${DentistId}`);
+  const res = await fetch(
+    `${process.env.NEXT_PUBLIC_BACKEND_URL}/dentists/${DentistId}`
+  );
 
-   if(!res.ok) {
-      throw new Error("Failed to fetch dentist");
-   }
+  if (!res.ok) {
+    throw new Error("Failed to fetch dentist");
+  }
 
-   return await res.json();
+  return await res.json();
 }
