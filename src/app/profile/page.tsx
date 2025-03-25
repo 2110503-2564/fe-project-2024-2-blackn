c07d71985287cf5a31e6b689e91f6f260188bcd0
Keerati Setthasuk <keerati.se123@gmail.com>
2025-03-25 13:36:48 +0700

    Refactor Profile page layout for improved spacing and consistency

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -64,16 +64,16 @@ export default function Profile() {
 
   return (
     <div className={`flex flex-col justify-center px-[1.5%] py-[1%] bg-gradient-to-b from-green-100 to-white min-h-screen max-md:px-5`}>
-      <div className={`px-28  bg-white rounded-3xl h-[calc(100vh-32px)] max-md:px-5 max-md:max-w-full shadow-xl transform /*hover:scale-[1.01]*/ transition-all duration-300`}>
+      <div className={`px-28  bg-white rounded-3xl h-[calc(100vh-2%)] max-md:px-5 max-md:max-w-full shadow-xl transform /*hover:scale-[1.01]*/ transition-all duration-300`}>
         <div className="flex gap-5 max-md:flex-col h-lvh">
           <div className="w-[39%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col items-start mt-[10%] w-full text-3xl text-black max-md:mt-10">
+            <div className="flex flex-col gap-[1%] overflow-auto h-full justify-between mt-[3%] w-full text-3xl text-black max-md:mt-10">
               <div className="ml-8 text-3xl text-center max-md:ml-2.5">
                 {user.name}'s Profile
               </div>
-              <div className="flex shrink-0 self-stretch mt-3.5 h-2 bg-sky-200 rounded-3xl" />
-              <div className="mt-[10%] max-md:mt-10">Name</div>
-              <div className={`flex gap-9 items-start mt-6 w-[70%] /*ml-11*/ whitespace-nowrap max-md:ml-2.5`}>
+              <div className={`flex shrink-0 self-stretch /*mt-3.5*/ h-2 bg-sky-200 rounded-3xl`} />
+              <div className={`/*mt-[10%]*/ max-md:mt-10`}>Name</div>
+              <div className={`flex gap-9 items-start /*mt-6*/ w-auto /*ml-11*/ whitespace-nowrap max-md:ml-2.5`}>
               <input
               className="w-full text-xl rounded-3xl bg-zinc-100 h-[62px] px-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
               name="name"
@@ -88,8 +88,8 @@ export default function Profile() {
                   className="object-contain shrink-0 mt-1 aspect-[1.11] w-[31px]"
                 /> */}
               </div>
-              <div className="mt-[16%] max-md:mt-10">Email</div>
-              <div className={`flex gap-10 mt-6 /*ml-11*/ w-[70%] whitespace-nowrap max-md:mt-10 max-md:ml-2.5`}>
+              <div className={`/*mt-[16%]*/ max-md:mt-10`}>Email</div>
+              <div className={`flex gap-10 /*mt-6*/ /*ml-11*/ w-auto whitespace-nowrap max-md:mt-10 max-md:ml-2.5`}>
                   <input
                   className="w-full text-xl rounded-3xl bg-zinc-100 h-[62px] px-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
                   name="email"
@@ -104,9 +104,9 @@ export default function Profile() {
                   className="object-contain shrink-0 aspect-[1.11] w-[31px]"
                 /> */}
               </div>
-              <div className="mt-[16%] max-md:mt-10">Phone Number</div>
-              <div className={`flex gap-10 /*ml-10*/ mt-6 w-[70%] whitespace-nowrap mb-[16%] max-md:mt-10`}>                
-                <div className="grow shrink my-auto w-[159px]">
+              <div className={`/*mt-[16%]*/ max-md:mt-10`}>Phone Number</div>
+              <div className={`flex gap-10 /*ml-10*/ /*mt-6*/ w-auto whitespace-nowrap /*mb-[16%]*/ max-md:mt-10`}>                
+                <div className="grow shrink my-auto w-auto">
                   <input
                     className="w-full text-xl rounded-3xl bg-zinc-100 h-[62px] px-6 focus:outline-none focus:ring-2 focus:ring-blue-500"
                     name="tel_number"
@@ -138,7 +138,7 @@ export default function Profile() {
             ""
           ) : (
             <div className="flex justify-end w-full max-md:ml-0 max-md:w-full">
-              <div className={`flex flex-col mt-2 px-12 pt-10 w-[90%] h-[94%] overflow-auto bg-gradient-to-r from-green-100 to-sky-100 rounded-3xl /*pb-[464px]*/ max-md:px-5 max-md:pb-24 max-md:mt-10 max-md:max-w-full shadow-lg transform /*hover:scale-[1.01]*/ transition-all duration-300`}>
+              <div className={`flex flex-col mt-2 px-12 pt-10 w-[90%] h-[98%] overflow-auto bg-gradient-to-r from-green-100 to-sky-100 rounded-3xl /*pb-[464px]*/ max-md:px-5 max-md:pb-24 max-md:mt-10 max-md:max-w-full shadow-lg transform /*hover:scale-[1.01]*/ transition-all duration-300`}>
                 <div className="self-center text-3xl font-extrabold text-black">
                   {user.role === "admin" ? "All Bookings" : `${user.name}'s Bookings`}
                 </div>
