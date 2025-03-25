6bc219651fa14d4b6ead893bf56c1fb60c86351a
ThawinXD <bto2168@gmail.com>
2025-03-25 10:46:25 +0700

    remove button in picture

--- src/components/DentistCard.tsx
+++ src/components/DentistCard.tsx
@@ -45,16 +45,6 @@ export const DentistCard: React.FC<DentistCardProps> = ({
       <div className="mt-5 text-3xl max-sm:text-2xl">
         {experience} Years of Experience
       </div>
-      <button
-        onClick={() => {
-          token
-            ? router.push("/booking?dentist=" + name)
-            : router.push("/login");
-        }}
-        className="absolute right-8 bottom-8 text-3xl bg-white border-black rounded-3xl h-[85px] w-[146px] max-md:relative max-md:right-auto max-md:bottom-auto max-md:mx-auto max-md:my-5 max-sm:w-full max-sm:text-2xl max-sm:h-[60px] hover:bg-gray-100 transition-colors flex items-center justify-center"
-      >
-        Booking
-      </button>
     </div>
   
     <div className="mt-6 text-lg text-center font-medium">{expertise}</div>
